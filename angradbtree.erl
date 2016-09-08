01abcaa11d7a8a7c997efaa27bfd23479acebdfb
Fabio Marques <140039082@aluno.unb.br>
2016-09-08 18:14:47 -0300

    Alterações feitas em angradbtree.erl

--- angradbtree.erl
+++ angradbtree.erl
@@ -1,4 +1,23 @@
 -module(angradbtree).
 -compile(export_all).
 
-save_doc() -> true.
+%	Função que salva um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
+%	A function that saves a document in the document's file. Receives as attribute: *attribute*. Returns: *return value*.
+save_doc(SizeInBits, Doc, DBName) -> 
+	{ok, Fp} = file:open(DBName, [write, append]),
+	DocBin = term_to_binary(Doc),
+	SizeBin = <<byte_size(Doc):SizeBits>>.
+	file:write(Fp, <<SizeBin/binary, DocBin/binary>>),
+	file:close(Fp).
+
+%	Função que lê um documento do arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
+%	A function that reads a document from the document's file. Receives as attribute: *attribute*. Returns: *return value*.
+read_doc() -> true.
+
+%	Função que atualiza um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
+%	A function that updates a document in the document's file. Receives as attribute: *attribute*. Returns: *return value*.
+update_doc() -> true.
+
+%	Função que exclui um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
+%	A function that deletes a document in document's file. Receives as attribute: *attribute*. Returns: *return value*.
+delete_doc() -> true.
