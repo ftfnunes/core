783a4c607805d00fab2d3c83014466cdd876deaf
Fabio Marques <140039082@aluno.unb.br>
2016-09-09 14:54:08 -0300

    Primeira versão do 'angradbtree' finalizada.

--- angradbtree.erl
+++ angradbtree.erl
@@ -2,22 +2,38 @@
 -compile(export_all).
 
 %	Função que salva um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
-%	A function that saves a document in the document's file. Receives as attribute: *attribute*. Returns: *return value*.
-save_doc(SizeInBits, Doc, DBName) -> 
+%	A function that saves a document in the document's file. Receives as attribute: *attribute*. Returns: *return value*. The document is received already in binary form and just has to be saved.  q
+create_doc(SizeInBytes, SizeVersion, Doc, DBName) -> 
 	{ok, Fp} = file:open(DBName, [write, append]),
-	DocBin = term_to_binary(Doc),
-	SizeBin = <<byte_size(Doc):SizeBits>>.
-	file:write(Fp, <<SizeBin/binary, DocBin/binary>>),
-	file:close(Fp).
+	{ok, Pos} = file:position(Fp, cur),
+	RawDocSize = byte_size(Doc),
+	SizeBin = <<RawDocSize:SizeInBytes/unit:8>>, % The Size is multiplied by 8 to obtain the value in bits.
+	Version = <<1:SizeVersion/unit:8>>, % Version is a 2 bytes integer, refering to the times that the doc was changed. "1" means first version. The Size is multiplied by 8 to obtain the value in bits.
+	PointerLastDoc = <<-1:4/unit:8>>
+	file:write(Fp, <<SizeBin/binary, Version/binary, Doc/binary, PointerLastDoc/binary>>),
+	file:close(Fp),
+	{ok, Pos}. % LEMBRAR DE QUE A PRIMEIRA VERSÃO DEVE SER COLOCADA NO ARQUIVO SUPERIOR!
 
 %	Função que lê um documento do arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
 %	A function that reads a document from the document's file. Receives as attribute: *attribute*. Returns: *return value*.
-read_doc() -> true.
+read_doc(PosInBytes, SizeInBytes, SizeVersion, DBName) -> 
+	{ok, Fp} = file:open(DBName, [read, binary]),
+	{ok, Fp2} = file:position(Fp, {bof, PosInBytes}),
+	{ok, DocSize} = file:read(Fp2, SizeInBytes),
+	{ok, Version} = file:read(Fp2, SizeVersion),
+	{ok, Doc} = file:read(Fp2, DocSize),
+	file:close(Fp),
+	{ok, Version, Doc}.
 
 %	Função que atualiza um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
 %	A function that updates a document in the document's file. Receives as attribute: *attribute*. Returns: *return value*.
-update_doc() -> true.
-
-%	Função que exclui um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
-%	A function that deletes a document in document's file. Receives as attribute: *attribute*. Returns: *return value*.
-delete_doc() -> true.
+update_doc(PosLastDoc, LastVersion, SizeInBytes, SizeVersion, Doc, DBName) ->
+	{ok, Fp} = file:open(DBName, [write, append]),
+	{ok, Pos} = file:position(Fp, cur),
+	RawDocSize = byte_size(Doc),
+	SizeBin = <<RawDocSize:SizeInBytes/unit:8>>, % The Size is multiplied by 8 to obtain the value in bits.
+	Version = <<(LastVersion+1):SizeVersion/unit:8>>, % Version is a 2 bytes integer, refering to the times that the doc was changed. "1" means first version. The Size is multiplied by 8 to obtain the value in bits.
+	PointerLastDoc = <<PosLastDoc:4/unit:8>>
+	file:write(Fp, <<SizeBin/binary, Version/binary, Doc/binary, PointerLastDoc/binary>>),
+	file:close(Fp),
+	{ok, Pos, (LastVersion+1)}.
