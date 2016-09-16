c73e444b4dd1dc6e98af1e74d3564a204b639426
ftfnunes <ftfnunes@hotmail.com>
2016-09-16 09:53:16 -0300

    bugs no salvamento do documento corrigido.

--- adbtree.erl
+++ adbtree.erl
@@ -1,4 +1,4 @@
--module(angradbtree).
+-module(adbtree).
 -include("adbtree.hrl").
 -compile(export_all).
 
@@ -6,10 +6,11 @@
 %	A function that reads a document from the document's file. Receives as attribute: *attribute*. Returns: *return value*.
 read_doc(PosInBytes, Settings) -> 
 	{ok, Fp} = file:open(Settings#dbsettings.dbname, [read, binary]),
-	{ok, Fp2} = file:position(Fp, {bof, PosInBytes}),
-	{ok, DocSize} = file:read(Fp2, Settings#dbsettings.sizeinbytes),
-	{ok, Version} = file:read(Fp2, Settings#dbsettings.sizeversion),
-	{ok, Doc} = file:read(Fp2, DocSize),
+	{ok, _NP} = file:position(Fp, {bof, PosInBytes}),
+	SizeSize = Settings#dbsettings.sizeinbytes,
+	{ok, <<DocSize:SizeSize/unit:8>>} = file:read(Fp, Settings#dbsettings.sizeinbytes),
+	{ok, Version} = file:read(Fp, Settings#dbsettings.sizeversion),
+	{ok, Doc} = file:read(Fp, DocSize),
 	file:close(Fp),
 	{ok, Version, Doc}.
 
@@ -18,19 +19,15 @@ read_doc(PosInBytes, Settings) ->
 %	A function that updates a document in the document's file. Receives as attribute: *attribute*. Returns: *return value*.
 save_doc(PosLastDoc, LastVersion, Doc, Settings) ->
 	{ok, Fp} = file:open(Settings#dbsettings.dbname, [write, append]),
-	{ok, Pos} = file:position(Fp, cur),
+	{ok, Pos} = file:position(Fp, eof),
 	RawDocSize = byte_size(Doc),
 	Size = Settings#dbsettings.sizeinbytes,
 	SizeBin = <<RawDocSize:Size/unit:8>>, % The Size is multiplied by 8 to obtain the value in bits.
 	SVersion = Settings#dbsettings.sizeversion,
 	Version = <<(LastVersion+1):SVersion/unit:8>>, % Version is a 2 bytes integer, refering to the times that the doc was changed. "1" means first version. The Size is multiplied by 8 to obtain the value in bits.
-	PointerLastDoc = <<PosLastDoc:4/unit:8>>
+	PointerLastDoc = <<PosLastDoc:4/unit:8>>,
 	file:write(Fp, <<SizeBin/binary, Version/binary, Doc/binary, PointerLastDoc/binary>>),
 	file:close(Fp),
 	{ok, Pos, (LastVersion+1)}.
 	% On create, remember to pass "-1" as PosLastDoc and "0" as LastVersion.
 
-
-%	Função que exclui um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
-%	A function that deletes a document in document's file. Receives as attribute: *attribute*. Returns: *return value*.
-delete_doc() -> true.
