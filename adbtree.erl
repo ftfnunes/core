6dc2db62c6c78412f37aa6a0304f2b0cc0c35245
ftfnunes <ftfnunes@hotmail.com>
2016-09-20 14:42:14 -0300

    create_function added.

--- adbtree.erl
+++ adbtree.erl
@@ -2,10 +2,13 @@
 -include("adbtree.hrl").
 -compile(export_all).
 
+-define(SizeOfPointer, 8).
+-define(SizeOfHeader, 64).
+
 %	Função que lê um documento do arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
 %	A function that reads a document from the document's file. Receives as attribute: *attribute*. Returns: *return value*.
 read_doc(PosInBytes, Settings) -> 
-	{ok, Fp} = file:open(Settings#dbsettings.dbname, [read, binary]),
+	{ok, Fp} = file:open((Settings#dbsettings.dbname ++ "Docs.adb"), [read, binary]),
 	{ok, _NP} = file:position(Fp, {bof, PosInBytes}),
 	SizeSize = Settings#dbsettings.sizeinbytes,
 	{ok, <<DocSize:SizeSize/unit:8>>} = file:read(Fp, Settings#dbsettings.sizeinbytes),
@@ -18,16 +21,35 @@ read_doc(PosInBytes, Settings) ->
 %	Função que atualiza um documento no arquivo de documentos. Recebe como parâmetro: *parâmetros* . Retorna: *retorno*.
 %	A function that updates a document in the document's file. Receives as attribute: *attribute*. Returns: *return value*.
 save_doc(PosLastDoc, LastVersion, Doc, Settings) ->
-	{ok, Fp} = file:open(Settings#dbsettings.dbname, [write, append]),
+	{ok, Fp} = file:open((Settings#dbsettings.dbname ++ "Docs.adb"), [write, append]),
 	{ok, Pos} = file:position(Fp, eof),
 	RawDocSize = byte_size(Doc),
 	Size = Settings#dbsettings.sizeinbytes,
 	SizeBin = <<RawDocSize:Size/unit:8>>, % The Size is multiplied by 8 to obtain the value in bits.
 	SVersion = Settings#dbsettings.sizeversion,
 	Version = <<(LastVersion+1):SVersion/unit:8>>, % Version is a 2 bytes integer, refering to the times that the doc was changed. "1" means first version. The Size is multiplied by 8 to obtain the value in bits.
-	PointerLastDoc = <<PosLastDoc:4/unit:8>>,
+	PointerLastDoc = <<PosLastDoc:?SizeOfPointer/unit:8>>,
 	file:write(Fp, <<SizeBin/binary, Version/binary, Doc/binary, PointerLastDoc/binary>>),
 	file:close(Fp),
 	{ok, Pos, (LastVersion+1)}.
 	% On create, remember to pass "-1" as PosLastDoc and "0" as LastVersion.
+
+
+%Add exception handling later...
+%Name is the name of the Database, without any extensions.
+%Header is formated as: NameOfDB/40bytes, SizeOfSizeOfDocs/8bytes,SyzeOfVersion/8bytes,PointerForRoot/8Bytes.
+create_db(Name, SizeOfSizeOfDoc, SizeOfVersion) -> 
+	Settings = #dbsettings{dbname=Name, sizeinbytes=SizeOfSizeOfDoc, sizeversion=SizeOfVersion},
+	NameBin = list_to_binary(lists:duplicate((40 - length(Name)), 0)++Name),
+	Header = <<NameBin/binary, SizeOfSizeOfDoc:8/unit:8, SizeOfVersion:8/unit:8, ?SizeOfHeader:?SizeOfPointer/unit:8>>,
+	NameIndex = Name++"Index.adb",
+	{ok, Fp} = file:open(NameIndex, [exclusive, binary, write]),
+	file:write(Fp, Header),
+	file:close(Fp),
+	Settings.
+
+
+
+
+
 
